0b88e4d03d304261dfa00efcdc27e98188822652
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-29 21:49:23 -0500

    componente del juego implementado

---
+++ src/app/components/GameSection.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import InteractiveGrid from './InteractiveGrid'
+
+const GameSection = () => {
+    return (
+        <div>
+            <InteractiveGrid />
+        </div>
+    )
+}
+
+export default GameSection
