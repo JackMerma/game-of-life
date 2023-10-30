2238e92dd6f79d1fff0d19a644bab3bdc9c347ec
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-29 21:56:34 -0500

    paquetes instalados y llamadas a los componentes

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
 import Image from 'next/image'
+import Sidebar from './components/Sidebar'
+import GameSection from './components/GameSection'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen">
+      <GameSection />
     </main>
   )
 }
