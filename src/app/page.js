f022b8443504628bfd4f31c158b6ea3c1652f7a7
Jackson Merma <mermaportocarreroj@gmail.com>
2023-10-30 00:24:31 -0500

    implementación del sidebar principal con la lista que se envia para los sidebaritems

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import Sidebar from './components/Sidebar'
 import GameSection from './components/GameSection'
+import SideBarSection from './components/SideBarSection'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen">
+      <SideBarSection />
       <GameSection />
     </main>
   )
