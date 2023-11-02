4e3a4faa133ca92ecb0334b1552a18ff090d5436
Jackson Merma <mermaportocarreroj@gmail.com>
2023-11-02 17:39:54 -0500

    agregando seccion para animacion 3d (creando el componente tambien)

--- src/app/components/GameSection.jsx
+++ src/app/components/GameSection.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import InteractiveGrid from './InteractiveGrid'
+import InteractiveGrid3D from './InteractiveGrid3D'
+import ButtonComponent from './ButtonComponent'
+
+import {
+	Box,
+	Grid2X2,
+} from "lucide-react"
 
 const GameSection = () => {
-    return (
-        <div style={{width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center'}} className='game-box'>
-            <InteractiveGrid />
-        </div>
-    )
+	const [is3D, setIs3D] = useState(false);
+
+	const toggle3D = () => {
+		setIs3D(!is3D)
+	};
+
+	return (
+		<div style={{width: '100%', height: '100vh', display: 'flex', flexDirextion: 'column', justifyContent: 'center', alignItems: 'center'}} className='game-box'>
+		<div style={{height: '100%', display: 'block'}} className='pt-10 mb-10'>
+		<button 
+		className='mr-3 bg-[#424242] hover:bg-[#212121] text-white mt-10 ml-4 rounded-full'
+		onClick={toggle3D}>
+		<span className="mt-auto block px-4 py-4">
+		{is3D ? <Grid2X2 size={25} /> : <Box size={25} />}
+		</span>
+		</button>
+		</div>
+
+		<div>
+		{is3D ? <InteractiveGrid3D /> : <InteractiveGrid />}
+		</div>
+		</div>
+	)
 }
 
 export default GameSection
---
+++ src/app/components/InteractiveGrid3D.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InteractiveGrid3D = () => {
+	return (
+		<div>InteractiveGrid3D</div>
+	)
+}
+
+export default InteractiveGrid3D
